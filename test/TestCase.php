5c9c58858fda48800a6bd1c2b9be7c2733de3f51
sc0Vu <alk03073135@gmail.com>
2018-02-16 01:47:53 +0800

    Add HexPrivateKeySerializer.php

---
+++ test/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Test;
+
+use \PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * testPrivateKey
+     * 
+     * @var string
+     */
+    protected $testPrivateKey = 'd0459987fdde1f41e524fddbf4b646cd9d3bea7fd7d63feead3f5dfce6174a3d';
+
+    /**
+     * setUp
+     * 
+     * @return void
+     */
+    public function setUp() {}
+
+    /**
+     * tearDown
+     * 
+     * @return void
+     */
+    public function tearDown() {}
+}
